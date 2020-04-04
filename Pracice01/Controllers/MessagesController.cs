a57cf1dac51b1f4752ab78fb6609f1ac41d551f0
Arina Issakina <arina.isakina@gmail.com>
2020-04-04 11:04:36 +0600

    commitp2

--- Pracice01/Controllers/MessagesController.cs
+++ Pracice01/Controllers/MessagesController.cs
@@ -2,11 +2,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Pracice01.Controllers
 {
+    [Route("Say")]
     public class MessagesController : Controller
     {
-        // GET
-        public IActionResult Index()
+        [Route("{**message}")]
+        public IActionResult ShowMessage(string message)
         {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+        
             return View();
         }
     }
